46a803c00f0906ede51b65d8572fe0f457c5e2b9
Sam Morrison <sorrison@gmail.com>
2019-06-19 14:32:00 +1000

    Switch to use xtrabackup for innodb backup/restore
    innodbbackup is not symlinked to xtrabackup for version 8 so
    this means we should use xtrabackup directly. It was already
    assumed that xtrabackup was in use by additional commands.
    
    Also update args to use non deprecated commands.
    
    Change-Id: Iabc9f2824ca03757931a40cac339c9f8e84d07a1

--- trove/guestagent/strategies/restore/mysql_impl.py
+++ trove/guestagent/strategies/restore/mysql_impl.py
@@ -39,7 +39,7 @@ class MySQLRestoreMixin(object):
     RESET_ROOT_SLEEP_INTERVAL = 10
 
     RESET_ROOT_MYSQL_COMMANDS = ("SET PASSWORD FOR "
-                                 "'root'@'localhost'=PASSWORD('');")
+                                 "'root'@'localhost'='';")
     # This is a suffix MySQL appends to the file name given in
     # the '--log-error' startup parameter.
     _ERROR_LOG_SUFFIX = '.err'
@@ -186,11 +186,10 @@ class InnoBackupEx(base.RestoreRunner, MySQLRestoreMixin):
     __strategy_name__ = 'innobackupex'
     base_restore_cmd = ('sudo xbstream -x -C %(restore_location)s'
                         ' 2>/tmp/xbstream_extract.log')
-    base_prepare_cmd = ('sudo innobackupex'
+    base_prepare_cmd = ('sudo xtrabackup'
                         ' --defaults-file=%(restore_location)s/backup-my.cnf'
-                        ' --ibbackup=xtrabackup'
-                        ' --apply-log'
-                        ' %(restore_location)s'
+                        ' --prepare'
+                        ' --target-dir=%(restore_location)s'
                         ' 2>/tmp/innoprepare.log')
 
     def __init__(self, *args, **kwargs):
@@ -217,9 +216,9 @@ class InnoBackupEx(base.RestoreRunner, MySQLRestoreMixin):
         utils.clean_out(self.restore_location)
 
     def _run_prepare(self):
-        LOG.debug("Running innobackupex prepare: %s.", self.prepare_cmd)
+        LOG.debug("Running xtrabackup prepare: %s.", self.prepare_cmd)
         self.prep_retcode = utils.execute(self.prepare_cmd, shell=True)
-        LOG.info("Innobackupex prepare finished successfully.")
+        LOG.info("Xtrabackup prepare finished successfully.")
 
     def post_restore(self):
         self._run_prepare()
@@ -274,12 +273,11 @@ class InnoBackupEx(base.RestoreRunner, MySQLRestoreMixin):
 
 class InnoBackupExIncremental(InnoBackupEx):
     __strategy_name__ = 'innobackupexincremental'
-    incremental_prep = ('sudo innobackupex'
+    incremental_prep = ('sudo xtrabackup'
                         ' --defaults-file=%(restore_location)s/backup-my.cnf'
-                        ' --ibbackup=xtrabackup'
-                        ' --apply-log'
-                        ' --redo-only'
-                        ' %(restore_location)s'
+                        ' --prepare'
+                        ' --apply-log-only'
+                        ' --target-dir=%(restore_location)s'
                         ' %(incremental_args)s'
                         ' 2>/tmp/innoprepare.log')
 
@@ -310,9 +308,9 @@ class InnoBackupExIncremental(InnoBackupEx):
 
     def _incremental_prepare(self, incremental_dir):
         prepare_cmd = self._incremental_prepare_cmd(incremental_dir)
-        LOG.debug("Running innobackupex prepare: %s.", prepare_cmd)
+        LOG.debug("Running xtrabackup prepare: %s.", prepare_cmd)
         utils.execute(prepare_cmd, shell=True)
-        LOG.info("Innobackupex prepare finished successfully.")
+        LOG.info("xtrabackup prepare finished successfully.")
 
     def _incremental_restore(self, location, checksum):
         """Recursively apply backups from all parents.
